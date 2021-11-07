86398818e2431e98745e793482f404cb879c5f9b
Jeff Korby <82245490+jeffkorby@users.noreply.github.com>
2021-11-07 18:51:45 -0500

    server-side mutations

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
-
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth')
 const routes = require('./routes');
 
 const app = express();
