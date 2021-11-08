9a3f4a51f382f31ed7d172315b8de26b9660419e
Jeff Korby <82245490+jeffkorby@users.noreply.github.com>
2021-11-07 19:07:44 -0500

    client side queries/ added apolloprovider to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,35 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { 
+  ApolloProvider ,
+  ApolloClient,
+  InMemoryCache,
+  CreateHttpLink } 
+  from '@apollo/client';
+import { setContext } from '@apollo/client';
+
+const httpLink = CreateHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const authLink = SetContext(( __, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return { 
+    headers: {
+      ...headers, authorization: token ? `Bearer ${token} : ''`,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+}); 
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +42,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth')
-const routes = require('./routes');
+//const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: Authmiddleware
+  context: authMiddleware
 });
 
 server.applyMidleWare({ app });
@@ -25,7 +25,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+//app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
